0cd0c08b22bbac411e8472d6aadc6f91c3e5bef9
Theodore Verge <theodore.verge@gmail.com>
2020-12-09 12:38:22 +0000

    ..

--- starter/script.js
+++ starter/script.js
@@ -17,7 +17,20 @@ if (navigator.geolocation)
     const latitude = position.coords.latitude
     const longitude = position.coords.longitude
     console.log(`https://www.google.co.uk/maps/@${latitude},${longitude}z`)
-},
-function() {
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+    },
+
+  function() {
   alert('Could not get coordinates');
 });
